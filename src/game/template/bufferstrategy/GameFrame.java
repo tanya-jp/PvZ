9ca74267626e435b15f65f103aaacd95c6678887
tanya-jp <t_djavaherpour@yahoo.com>
2021-01-18 19:12:35 +0330

    "flowers added"

--- src/game/template/bufferstrategy/GameFrame.java
+++ src/game/template/bufferstrategy/GameFrame.java
@@ -20,13 +20,20 @@ public class GameFrame extends JFrame {
 
     public static final int GAME_HEIGHT = 772;
     public static final int GAME_WIDTH = 1010;
-    private final Image sun;
-    private final Image peaShooterCard;
-    private final Image sunFlowerCard;
-    private final Image cherryBombCard;
-    private final Image wallNutCard;
-    private final Image freezePeaShooterCard;
-    private final Image lawnMower;
+    private int x = 61;
+    private int y = 149;
+    private Image sun;
+    private Image peaShooterCard;
+    private Image sunFlowerCard;
+    private Image cherryBombCard;
+    private Image wallNutCard;
+    private Image freezePeaShooterCard;
+    private Image lawnMower;
+    private Image walnutFull;
+    private Image sunFlowerFull;
+    private Image peaShooterFull;
+    private Image freezeShooterFull;
+    private Image cherryFull;
     private String type;
 
     private BufferStrategy bufferStrategy;
@@ -40,6 +47,14 @@ public class GameFrame extends JFrame {
         //
         // Initialize the JFrame ...
         //
+        setImages();
+    }
+
+    /**
+     * Sets all images and gifs
+     */
+    private void setImages()
+    {
         sun = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\sun.gif").getImage();
         peaShooterCard = new ImageIcon(".\\PVS Design Kit\\images\\Cards\\card_peashooter.png").getImage();
         sunFlowerCard = new ImageIcon(".\\PVS Design Kit\\images\\Cards\\card_sunflower.png").getImage();
@@ -47,6 +62,11 @@ public class GameFrame extends JFrame {
         wallNutCard = new ImageIcon(".\\PVS Design Kit\\images\\Cards\\card_wallnut.png").getImage();
         freezePeaShooterCard = new ImageIcon(".\\PVS Design Kit\\images\\Cards\\card_freezepeashooter.png").getImage();
         lawnMower = new ImageIcon(".\\PVS Design Kit\\images\\Lawn_Mower.png").getImage();
+        walnutFull = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\walnut_full_life.gif").getImage();
+        sunFlowerFull = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\sun_flower.gif").getImage();
+        peaShooterFull = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\pea_shooter.gif").getImage();
+        freezeShooterFull = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\freezepeashooter.gif").getImage();
+        cherryFull = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\newCherryBomb.gif").getImage();
     }
 
     /**
@@ -88,6 +108,26 @@ public class GameFrame extends JFrame {
         GameCanvas canvas = new GameCanvas();
         canvas.paintComponent(g2d);
         //set cards
+        putCards(g2d, state);
+        //set lawn mowers
+        putLawnMower(g2d, state);
+        //set flowers
+        setFlowers(g2d, state);
+        //set sun
+        setSun(g2d, state);
+
+//        canvas.render(state);
+        //
+        // Draw all game elements according
+        //  to the game 'state' using 'g2d' ...
+        //
+    }
+
+    /**
+     * Shows cards based on the game state
+     */
+    private void putCards(Graphics2D g2d, GameState state)
+    {
         if(!state.getPea() && state.getSunNumber() >= 100)
             g2d.drawImage(peaShooterCard, 110, 38, null);
         if(!state.getSunFlower() && state.getSunNumber() >= 50)
@@ -98,24 +138,65 @@ public class GameFrame extends JFrame {
             g2d.drawImage(wallNutCard, 110+3*peaShooterCard.getWidth(null), 38, null);
         if(!state.getFreezePea() && state.getSunNumber() >= 175)
             g2d.drawImage(freezePeaShooterCard, 110+4*peaShooterCard.getWidth(null), 38, null);
-        //set lawn mowers
+    }
+
+    /**
+     * Shows lawnMowers based on the game state
+     */
+    private void putLawnMower(Graphics2D g2d, GameState state)
+    {
         g2d.drawImage(lawnMower,-35,150,null);
         g2d.drawImage(lawnMower,-35,150 + 120,null);
         g2d.drawImage(lawnMower,-35,150 + 120*2,null);
         g2d.drawImage(lawnMower,-35,150 + 120*3,null);
         g2d.drawImage(lawnMower,-35,150 + 120*4,null);
         g2d.drawImage(lawnMower,-35,150 + 120*5,null);
+    }
+
+    /**
+     * Shows sun and the number of caught suns based on the game state
+     */
+    private void setSun(Graphics2D g2d, GameState state)
+    {
         long start = System.currentTimeMillis();
         long delay = (1000 / 30) - (System.currentTimeMillis() - start);
         if(delay > 0 && !state.getSun())
             g2d.drawImage(sun, state.sunX, state.sunY, null);
         g2d.setFont(g2d.getFont().deriveFont(18.0f));
         g2d.drawString(String.valueOf(state.sunNumber), 55, 125);
-//        canvas.render(state);
-        //
-        // Draw all game elements according
-        //  to the game 'state' using 'g2d' ...
-        //
+    }
+
+    /**
+     *Show flowers on the playground
+     */
+    private void setFlowers(Graphics2D g2d, GameState state)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for(int j = 1; j <= 5; j++)
+            {
+                int loc = j*10 + i;
+                int locX = 0, locY = 0;
+                if(state.getInfo().get(loc) != null)
+                {
+                    locX = x + (i-1)*100;
+                    locY = y + (j-1)*115;
+
+                    if(state.getInfo().get(loc).equals("peaShooter"))
+                        g2d.drawImage(peaShooterFull, locX, locY, null);
+                    else if(state.getInfo().get(loc).equals("sunFlower"))
+                        g2d.drawImage(sunFlowerFull, locX, locY, null);
+                    else if(state.getInfo().get(loc).equals("cherryBomb"))
+                        g2d.drawImage(cherryBombCard, locX, locY, null);
+                    else if(state.getInfo().get(loc).equals("wallNut"))
+                        g2d.drawImage(walnutFull, locX, locY, null);
+                    else if(state.getInfo().get(loc).equals("freezePeaShooter"))
+                        g2d.drawImage(freezeShooterFull, locX, locY, null);
+
+                }
+
+            }
+        }
     }
 
 }
