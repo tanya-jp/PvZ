eea43a80a91f89022b865624c6c33d3c341d358c
tanya-jp <t_djavaherpour@yahoo.com>
2021-01-23 19:30:56 +0330

    "Squash attacks"

--- src/game/template/Elements/ConeHeadZombie.java
+++ src/game/template/Elements/ConeHeadZombie.java
@@ -6,9 +6,14 @@ import java.awt.*;
 public class ConeHeadZombie implements Images, Zombies{
     private Image fullZombie;
     private Image burntZombie;
+    private Image dyingZombie;
     private int life;
     private long stopTime;
+    private long burntTime;
+    private long squashAttackTime;
     private boolean stopped;
+    private boolean burnt;
+    private boolean squashAttacked;
     //1-9
     private int row;
     private float x;
@@ -18,6 +23,8 @@ public class ConeHeadZombie implements Images, Zombies{
         setImages();
         life = 560;
         stopped= false;
+        squashAttacked = false;
+        burnt = false;
     }
     /**
      * Sets the row of this zombie
@@ -76,6 +83,7 @@ public class ConeHeadZombie implements Images, Zombies{
     public void setImages(){
         fullZombie = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\coneheadzombie.gif").getImage();
         burntZombie = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\burntZombie.gif").getImage();
+        dyingZombie = new ImageIcon(".\\PVS Design Kit\\images\\Gifs\\zombie_normal_dying.gif").getImage();
     }
     /**
      * Returns the image of full flower
@@ -84,6 +92,11 @@ public class ConeHeadZombie implements Images, Zombies{
     public Image getFullImage(){
         return fullZombie;
     }
+    /**
+     * Returns the image of dying zombie
+     */
+    @Override
+    public Image getDyingImage(){return dyingZombie;}
     /**
      * Returns image of burnt zombie
      */
@@ -121,4 +134,57 @@ public class ConeHeadZombie implements Images, Zombies{
     public long getStopTime() {
         return stopTime;
     }
+    /**
+     * When cherryBomb burns zombie, sets that this zombie has been burnt.
+     * @param burnt -> true if zombie has been burnt.
+     */
+    @Override
+    public void setBurnt(boolean burnt) {
+        this.burnt = burnt;
+    }
+    /**
+     * Returns true if this zombie has been burnt.
+     */
+    @Override
+    public boolean isBurnt() {
+        return burnt;
+    }
+    /**
+     * Sets the time that cherryBomb burt zombie
+     */
+    @Override
+    public void setBurntTime(long burntTime) {
+        this.burntTime = burntTime;
+    }
+    /**
+     * Gets the time that cherryBomb burt zombie
+     * @return burtTime
+     */
+    @Override
+    public long getBurntTime() {
+        return burntTime;
+    }
+    /**
+     * when squash jumps on this zombie squashAttack should be true
+     */
+    @Override
+    public void setSquashAttacked(boolean squashAttacked){this.squashAttacked = squashAttacked;}
+    /**
+     * Returns if this flower has been attacked by squash
+     * @return true when has been attacked by squash
+     */
+    @Override
+    public boolean isSquashAttacked(){return squashAttacked;}
+    /**
+     * Sets the time of squash jumping
+     * @param squashAttackTime as time of starting attack.
+     */
+    @Override
+    public void setSquashAttackTime(long squashAttackTime){this.squashAttackTime = squashAttackTime;}
+    /**
+     * Gets the time of squash jumping
+     * @return squashAttackTime as time of starting attack.
+     */
+    @Override
+    public long getSquashAttackTime(){return squashAttackTime;}
 }
--- src/game/template/Elements/Zombies.java
+++ src/game/template/Elements/Zombies.java
@@ -43,7 +43,49 @@ public interface Zombies {
      */
     long getStopTime();
     /**
+     * Returns the image of dying zombie
+     */
+    Image getDyingImage();
+    /**
      * Returns image of burnt zombie
      */
     Image getBurntImage();
+    /**
+     * When cherryBomb burns zombie, sets that this zombie has been burnt.
+     * @param burnt -> true if zombie has been burnt.
+     */
+    void setBurnt(boolean burnt);
+    /**
+     * Returns true if this zombie has been burnt.
+     */
+    boolean isBurnt();
+    /**
+     * Sets the time that cherryBomb burt zombie
+     */
+    void setBurntTime(long burntTime);
+    /**
+     * Gets the time that cherryBomb burt zombie
+     * @return burtTime
+     */
+    long getBurntTime();
+    /**
+     * when squash jumps on this zombie squashAttack should be true
+     */
+    void setSquashAttacked(boolean squashAttacked);
+    /**
+     * Returns if this flower has been attacked by squash
+     * @return true when has been attacked by squash
+     */
+    boolean isSquashAttacked();
+    /**
+     * Sets the time of squash jumping
+     * @param squashAttackTime as time of starting attack.
+     */
+    void setSquashAttackTime(long squashAttackTime);
+    /**
+     * Gets the time of squash jumping
+     * @return squashAttackTime as time of starting attack.
+     */
+    long getSquashAttackTime();
+
 }
