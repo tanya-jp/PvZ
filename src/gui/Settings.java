2da68026e72fd80ddc082ca07d28176442e7b752
eakbari <elahehak@aut.ac.ir>
2021-01-25 12:48:00 +0330

    "Add some settings buttons"

--- src/gui/Settings.java
+++ src/gui/Settings.java
@@ -1,4 +1,21 @@
 package gui;
 
+import javax.swing.*;
+
 public class Settings {
+
+    //buttons
+    private JButton modeButton;
+    private JButton typeButton;
+    private JButton soundButton;
+    private JButton choosePlantsButton;
+    private JButton okButton;
+
+    public Settings(){
+        modeButton = new JButton();
+        typeButton = new JButton();
+        soundButton = new JButton();
+        choosePlantsButton = new JButton();
+        okButton = new JButton();
+    }
 }
